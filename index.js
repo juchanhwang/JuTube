bf2ed108851cbdaaea4ddadad23caf747479b4b3
juchanhwang <ghkdwncks96@gmail.com>
2019-07-11 16:22:52 +0900

    refactoring index.js

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
-const express = require("express");
+import express from "express";
+// const express = require("express");
+
 const app = express();
 
 const PORT = 4000;
 
-function handleListening() {
+const handleListening = () => {
   console.log(`Listening on: http://localhost:${PORT}`);
-}
+};
 
-function handleHome(req, res){
-    res.send('Hello from home');
-}
+const handleHome = (req, res, next) => res.send("Hello from home");
 
+const handleProfile = (req, res, next) => res.send("You are on my profile");
 
-function handleProfile(req, res){
-    res.send("You are on my profile")
-}
+const betweenHome = (req, res, next) => {
+  console.log("between");
+  next();
+};
 
-app.get("/", handleHome);
+// app.use(betweenHome);
+
+app.get("/",betweenHome, handleHome);
 
 app.get("/profile", handleProfile);
+
 
 app.listen(PORT, handleListening);
--- lectureSummary/ExpressJS.md
+++ lectureSummary/ExpressJS.md
@@ -44,3 +44,14 @@ app.listen(PORT, handleListening);
 
 > 프로젝트에 필요없는  dependency는 `npm install nodemon -D` 뒤에 `-D` 를 붙여준다. 개발자에게 필요한 devDependencies에 저장한다. 
 
+
+
+
+
+## 2.6 Express Core: Middlewares
+
+express에서 middleware란 처리가 끝날 때까지 연결되어 있는 것이다. 
+
+- request가 어떻게 되는가
+  - 웹 사이트에 접근하려고하는 순간이 시작
+- 유저의 home(/) 요청과 handleHome 사이에 있는 것 request한 것에 대한 권한 또한 필요하다. 요청을 처리할 수 있는 권한을 줘야함 ==> next()
